6e1c2d1f2e8eb43a423a30b5ab28a47cfaff2b2e
Akshata Kodak <85898174+akshatak1105@users.noreply.github.com>
2024-01-25 11:14:23 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import com.example.demo.model.SessionManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+	SpringApplication.run(ApiApplication.class, args);
+		
+		//SessionManagement obj=new SessionManagement();
+		//obj.signUp();
+		
+	
+	}
+
+}
