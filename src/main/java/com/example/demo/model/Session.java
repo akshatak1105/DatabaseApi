509b405cd4d417f69b54d9a1b92406f26b60c685
Akshata Kodak <85898174+akshatak1105@users.noreply.github.com>
2024-02-01 10:10:59 +0530

    Update Session.java

--- src/main/java/com/example/demo/model/Session.java
+++ src/main/java/com/example/demo/model/Session.java
@@ -6,6 +6,30 @@ import java.util.UUID;
 public class Session {
 	String connectionString;
 	UUID uuid;
+	public String getConnectionString() {
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public LocalDateTime getCurrentDateTime() {
+		return currentDateTime;
+	}
+
+	public void setCurrentDateTime(LocalDateTime currentDateTime) {
+		this.currentDateTime = currentDateTime;
+	}
+
 	LocalDateTime currentDateTime;
 
 	Session(String connectionString) {
