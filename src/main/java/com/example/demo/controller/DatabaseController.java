a78d4281cdcbe3906ceb9de5a74238c6dcb710e0
Akshata Kodak <85898174+akshatak1105@users.noreply.github.com>
2024-02-01 10:06:18 +0530

    Update DatabaseController.java

--- src/main/java/com/example/demo/controller/DatabaseController.java
+++ src/main/java/com/example/demo/controller/DatabaseController.java
@@ -1,18 +1,29 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.model.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") 
 @RequestMapping("/api/database")
 public class DatabaseController {
 	
+//	@PostMapping("/connect")
+//	public  String connect(@RequestParam String url) {
+//		String guid =  SessionManagement.add(url);
+//		return guid;
+//		
+//	}
+	
 	@PostMapping("/connect")
-	public static String connect(@RequestBody String connectionString) {
-		String guid =  SessionManagement.add(connectionString);
-		return guid;
-		
+	public ResponseEntity<String> connect(@RequestParam String url) {
+	    // Your logic here
+	    String guid = SessionManagement.add(url);
+	    String response = "{ \"token\" : \"" + guid + "\"}";
+	    return ResponseEntity.ok(response);
 	}
 
 }
 
+ 
