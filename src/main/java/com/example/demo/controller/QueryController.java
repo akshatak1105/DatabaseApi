c8591495ee6af644a7d679a8a01dc9cf4053f4b2
Akshata Kodak <85898174+akshatak1105@users.noreply.github.com>
2024-02-01 10:06:47 +0530

    Update QueryController.java

--- src/main/java/com/example/demo/controller/QueryController.java
+++ src/main/java/com/example/demo/controller/QueryController.java
@@ -3,16 +3,16 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.QueryExecution;
+import com.example.demo.model.*;
 
 @RestController
 public class QueryController {
-	
+
 	@PostMapping("/execute")
 	public static String execute(@RequestBody QueryExecution query) {
-		return query.getQuery();
-		
+		String connectionString = SessionManagement.find(query.getUiud());
+		String result = DataAccess.executeQuery(connectionString, query.getQuery());
+		return result;
 	}
 
 }
